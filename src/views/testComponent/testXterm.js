77199f1e8eb71ea734d8e212418abc952dc32287
wangchuangQcraft <wangchaung@qcraft.ai>
2022-03-22 18:59:40 +0800

    基本实现终端

--- src/views/testComponent/testXterm.js
+++ src/views/testComponent/testXterm.js
@@ -6,11 +6,10 @@ import 'xterm/css/xterm.css';
 import {useEffect, useState} from "react";
 
 export function TestXterm () {
+    const [terminal, setTerminal] = useState(null)
     const [inputString, setInputString] = useState('')
-    useEffect(() => {
-        handleClick()
-    }, [])
-    const handleClick = () => {
+
+    const terminalInit = () => {
         const term = new Terminal({
             rendererType: 'canvas',
             convertEol: true,
@@ -25,30 +24,136 @@ export function TestXterm () {
                 lineHeight: 20
             },
         })
-
         // 创建实例
         term.open(document.getElementById('terminal'))
         // 适配continue 元素
         const fitAddon = new FitAddon()
         term.loadAddon(fitAddon)
+        // 初始化输出
         term.writeln('\x1b[1;1;32mwellcom to web terminal!\x1b[0m')
         const prefix = 'admin $'
         term.write(prefix)
+        setTerminal(term)
+    }
 
-        // 处理按键
-        let str = ''
-        term.onKey((e) => {
+    useEffect(() => {
+        terminalInit()
+    }, [])
+
+
+
+    /*
+    *  terminal 的输入配置
+    *
+    * **/
+    let str = ''
+    const prefix = 'admin $'
+    const TERMINAL_INPUT_KEY = {
+        BACK: 8, // 退格删除键
+        ENTER: 13, // 回车键
+        UP: 38, // 方向盘上键
+        DOWN: 40, // 方向下键
+        LEFT: 37, // 方向盘左键
+        RIGHT: 39 // 方向盘右键
+    }
+
+    /**
+     * 获取 偏移长度
+     * */
+    const getCursorOffsetLength = (offsetLength, subString) => {
+        let cursorOffsetLength = ''
+        for (let i = 0; i < offsetLength; i++) {
+            cursorOffsetLength = cursorOffsetLength + subString
+        }
+        return cursorOffsetLength
+    }
+
+    // 按下回车处理方法
+    let currentIndex = 0
+    let inputTextList = []
+    const handleInputText = () => {
+        if (!str.trim()) {
+            terminal.write(`\r\n${prefix}`)
+            return
+        }
+        if (inputTextList.indexOf(str) === -1) {
+            inputTextList.push(str)
+            currentIndex = inputTextList.length
+        }
+        terminal.write(`\r\n${prefix}`)
+    }
+
+    /**
+     * 自动换行
+     * */
+    const preStrList = []
+    const handleAutoWarp = () => {
+        preStrList.push(str)
+        terminal.write('\n')
+        str = ''
+    }
+
+    // 添加键入事件
+    const handleOnKeyAction = () => {
+        terminal.onKey((e) => {
             const { key, domEvent } = e
-            term.write(key)
-            str = str + key
-            let totalOffsetLength = str.length + prefix.length   // 光标总偏移量
-            const currentOffsetLength = term._core.buffer.x // 光标当前偏移量
-            console.log(totalOffsetLength)
-            console.log(currentOffsetLength)
+            const { keyCode } = domEvent
+            const totalOffsetLength = str.length + prefix.length // 总偏移量
+            const currentOffsetLength = terminal._core.buffer.x // 当前 x 偏移量
 
+            switch(true) {
+                case totalOffsetLength >= terminal.cols - 1: // 总偏移量大于一行的宽度， TODO 需要换行
+                    console.log(1)
+                    handleAutoWarp()
+                    break
+                case keyCode === TERMINAL_INPUT_KEY.ENTER: // 按下回车
+                    handleInputText()
+                    str = ''
+                    break
+                case keyCode === TERMINAL_INPUT_KEY.BACK: // 删除键
+                    if (currentOffsetLength > prefix.length) {
+                        const cursorOffSetLength = getCursorOffsetLength(totalOffsetLength - currentOffsetLength, '\x1b[D') // 保留原来光标位置
+                        terminal._core.buffer.x = currentOffsetLength - 1
+                        terminal.write('\x1b[?K' + str.slice(currentOffsetLength - prefix.length))
+                        terminal.write(cursorOffSetLength)
+                        str = `${str.slice(0, currentOffsetLength - prefix.length - 1)}${str.slice(currentOffsetLength - prefix.length)}`
+                    }
+                    break
+                case keyCode === TERMINAL_INPUT_KEY.LEFT: // 左方向键
+                    if (currentOffsetLength > prefix.length) {
+                        terminal.write(key) // '\x1b[D'
+                    }
+                    break
+                case keyCode === TERMINAL_INPUT_KEY.RIGHT: // 右方向键
+                    if (currentOffsetLength < totalOffsetLength) {
+                        terminal.write(key) // '\x1b[C'
+                    }
+                    break
+                case [TERMINAL_INPUT_KEY.UP, TERMINAL_INPUT_KEY.DOWN].includes(keyCode): // 方向上键，下键
+                    break
+                case currentOffsetLength >= totalOffsetLength:  // 当前光标位置（x偏移量）>= 当前已经输入内容的总长度，则继续写入内容
+                    console.log(3)
+                    terminal.write(key)
+                    str = str + key
+                    break
+                case currentOffsetLength < totalOffsetLength: // 当前光标位置 < 已输入内容长度之前
+                    const cursorOffSetLength = getCursorOffsetLength(totalOffsetLength - currentOffsetLength, '\x1b[D') // 记录光标位置
+                    terminal.write(`${key}${str.slice(-(totalOffsetLength - currentOffsetLength))}`)
+                    terminal.write(cursorOffSetLength) // 将光标移动位置
+                    str = str.slice(0, totalOffsetLength - currentOffsetLength - 1 ) + key + str.slice(totalOffsetLength - currentOffsetLength - 1)
+                    break
+            }
 
         })
     }
+
+    useEffect(() => {
+        if (terminal) {
+            handleOnKeyAction()
+        }
+    }, [terminal])
+
+
 
 
         return (
